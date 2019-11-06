efcf98c7be0528c8989a583ccd65ecdb7ea86c26
NaoOtosaka <1049798454@qq.com>
2019-11-06 03:39:49 +0000

    初始化样式

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -1,8 +1,21 @@
 <html>
 <head>
     <title>@yield('title', 'Weibo App')</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-    @yield('content')   <!--用于显示content区块的内容，内容由继承自该模板的子视图定义-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">Weibo</a>
+            <ul class="navbar-nav justify-content-end">
+                <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">登陆</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        @yield('content')   <!--用于显示content区块的内容，内容由继承自该模板的子视图定义-->
+    </div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -9,5 +9,16 @@
 @extends('layout.default')
 
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello</h1>
+        <p class="lead">
+            这里是基于<a href="https://learnku.com/">Laravel</a>的个人项目主页
+        </p>
+        <p>
+            小老弟，你想有一个发言讨论的平台吗？
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
