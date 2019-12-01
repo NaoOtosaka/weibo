c9443adc0b1a69c78f39019178034ac251525a34
NaoOtosaka <1049798454@qq.com>
2019-12-01 13:01:43 +0000

    首页动态流

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,7 +76,11 @@ class User extends Authenticatable
 
 //    获取动态流
     public function feed(){
-        return $this->statuses()
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+
+        return Status::whereIn('user_id', $user_ids)
+            ->with('user')
             ->orderBy('created_at', 'desc');
     }
 
